2d41ceb446ab4108844e6e374d2720356468600b
igorgcosta <igorgabriel@outlook.com>
2017-08-13 13:01:57 -0300

    adaptacao kinghost

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import http from 'http';
 import path from 'path'
 import logger from 'morgan'
 import cookieParser from 'cookie-parser'
@@ -57,6 +58,71 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const port = normalizePort(process.env.PORT_APP || '3000');
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log('Server running at :' + port);
+console.log('process.env.PORT_APP', process.env.PORT_APP);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+//  debug('Listening on ' + bind);
+}
 
 //export default app;
 module.exports = app;
