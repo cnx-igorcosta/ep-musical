3d8830d15b63492ab39acb6b2504dd375afec958
igorgcosta <igorgabriel@outlook.com>
2017-08-27 12:11:04 -0300

    grade de programacao

--- app/public/assets/js/angular/programacao-ctrl.js
+++ app/public/assets/js/angular/programacao-ctrl.js
@@ -11,18 +11,36 @@ App.controller('programacaoCtrl', function($scope, $resource, $base64){
   prgCtrl.mensagem = '';
   prgCtrl.logo = null;
   prgCtrl.programa = {};
-  prgCtrl.programas = [];
+  prgCtrl.programas = [
+    {dia: 2, dia_nome: 'SEGUNDA', programas: []},
+    {dia: 3, dia_nome: 'TERÇA', programas: []},
+    {dia: 4, dia_nome: 'QUARTA', programas: []},
+    {dia: 5, dia_nome: 'QUINTA', programas: []},
+    {dia: 6, dia_nome: 'SEXTA', programas: []},
+    {dia: 7, dia_nome: 'SÁBADO', programas: []},
+    {dia: 1, dia_nome: 'DOMINGO', programas: []}
+  ];
   prgCtrl.programa.dia_semana = 0;
-  prgCtrl.dias = [
+  prgCtrl.diasSelect = [
     {"id": 0, "label": "Selecione o dia da semana"},
-    {"id": 1, "label": "Domingo"},
     {"id": 2, "label": "Segunda"},
     {"id": 3, "label": "Terça"},
     {"id": 4, "label": "Quarta"},
     {"id": 5, "label": "Quinta"},
     {"id": 6, "label": "Sexta"},
-    {"id": 7, "label": "Sábado"}
+    {"id": 7, "label": "Sábado"},
+    {"id": 1, "label": "Domingo"}
   ];
+  prgCtrl.diasSemanaNav = [
+    {"id": 2, "label": "SEGUNDA"},
+    {"id": 3, "label": "TERÇA"},
+    {"id": 4, "label": "QUARTA"},
+    {"id": 5, "label": "QUINTA"},
+    {"id": 6, "label": "SEXTA"},
+    {"id": 7, "label": "SÁBADO"},
+    {"id": 1, "label": "DOMINGO"}
+  ];
+  prgCtrl.diaNav = 2;//SEGUNDAs
   prgCtrl.isDetalhar = false;
 
   prgCtrl.iniciar = function(){
@@ -80,17 +98,67 @@ App.controller('programacaoCtrl', function($scope, $resource, $base64){
     prgCtrl.programas = [];
     prgCtrl.mensagem = '';
     ProgramacaoService.query(function(retorno){
-        prgCtrl.programas = retorno;
-        prgCtrl.base64ToImage();
+        prgCtrl.base64ToImage(retorno);
+        prgCtrl.programas = prgCtrl.organizarProgramasPorDiaHora(retorno);
+        prgCtrl.preencherVazios();
     }, prgCtrl.tratarErro);
   };
 
-  prgCtrl.base64ToImage = function() {
-    for(var i = 0; i < prgCtrl.programas.length; i++) {
-      var programa = prgCtrl.programas[i];
-      if(programa.logo)
-        programa.logo = programa.logo.replace(/data:?image\/(jpeg|png|jpg);?base64,?/,'');
+  prgCtrl.organizarProgramasPorDiaHora = function(listaPrograma) {
+    var listaSemana = [
+      {dia: 1, dia_nome: 'DOMINGO', programas: []},
+      {dia: 2, dia_nome: 'SEGUNDA', programas: []},
+      {dia: 3, dia_nome: 'TERÇA', programas: []},
+      {dia: 4, dia_nome: 'QUARTA', programas: []},
+      {dia: 5, dia_nome: 'QUINTA', programas: []},
+      {dia: 6, dia_nome: 'SEXTA', programas: []},
+      {dia: 7, dia_nome: 'SÁBADO', programas: []},
+    ];
+
+    for(var i = 0; i < listaPrograma.length; i++) {
+      var prg = listaPrograma[i];
+      for(var j = 0; j < listaSemana.length; j++) {
+        var semana = listaSemana[j];
+        if(prg.dia_semana == semana.dia) {
+          semana.programas.push(prg);
+        }
+      }
     }
+    return listaSemana;
+  };
+
+  prgCtrl.base64ToImage = function(listaPrograma) {
+    for(var i = 0; i <listaPrograma.length; i++) {
+      var programa = listaPrograma[i];
+      if(programa.logo) {
+        programa.logo = programa.logo.replace(/data:?image\/(jpeg|png|jpg);?base64,?/,'data:image/jpeg;base64,');
+      } else{
+          programa.logo = './images/default-programacao.jpg';
+      }
+    }
+  };
+
+  prgCtrl.preencherVazios = function() {
+    var quantMaxPrg = 6;
+    for(var i = 0; i < prgCtrl.programas.length; i++) {
+      var semana = prgCtrl.programas[i];
+      if(semana.programas.length < quantMaxPrg) {
+        var faltam =  quantMaxPrg - semana.programas.length;
+        var progrmamaADefinir = {
+          nome: '',
+          hora_inicial: 'A DEFINIR',
+          descricao:'',
+          logo: './images/default-programacao.jpg'
+        };
+        for(var j = 0; j < faltam; j++) {
+          semana.programas.push(progrmamaADefinir);
+        }
+      }
+    }
+  };
+
+  prgCtrl.mostrarDia = function(idDia) {
+    prgCtrl.diaNav = idDia;
   }
 
   prgCtrl.deletar = function(programa){
@@ -120,6 +188,7 @@ App.controller('programacaoCtrl', function($scope, $resource, $base64){
     prgCtrl.logo = null;
     prgCtrl.programa = {};
     prgCtrl.programa.dia_semana = 0;
+    prgCtrl.diaNav = 2
     prgCtrl.isDetalhar = false;
     prgCtrl.listar();
   };
@@ -143,6 +212,7 @@ App.controller('programacaoCtrl', function($scope, $resource, $base64){
      r.readAsDataURL(f); //once defined all callbacks, begin reading the file
    };
 
+   prgCtrl.preencherVazios();
    prgCtrl.iniciar();
 });
 
